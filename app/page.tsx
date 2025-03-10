e76f909ccda4a8084e347edf04520b576f9af837
LostWeb45 <kostik052005@gmail.com>
2025-03-10 14:38:29 +0300

    Создание шапки

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <>
-      {" "}
-      <Button variant={"outline"}>Кнопка</Button>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-    </>
-  );
+  return <h1>Главная </h1>;
 }
