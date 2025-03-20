64a65a6171c7201eabac5b886c8ce31e4bf14ef6
LostWeb45 <kostik052005@gmail.com>
2025-03-20 14:45:45 +0300

    Фильтрация

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { Container } from "./container";
 import Image from "next/image";
 import { Button } from "../ui";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
 
 interface Props {
   className?: string;
@@ -16,14 +17,33 @@ export const Header: React.FC<Props> = ({ className }) => {
         <div className="flex items-center gap-4">
           <Image src="/logo.png" alt="Logo" width={32} height={32}></Image>
           <div>
-            <h1 className="text-2xl uppercase font-black">NextPizza</h1>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
             <p className="text-sm text-gray-400 leading-3">
               вкусней уже некуда
             </p>
           </div>
-          {/* Правая часть */}
-          <div className="flex items-center gap-3">
-            <Button variant="outline">Войти</Button>
+        </div>
+
+        {/* Правая часть */}
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+
+          <div>
+            <Button className="group relative">
+              <p>520 ₽</p>
+              <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart size={16} className="relative" strokeWidth={2} />
+                <b>2</b>
+              </div>
+              <ArrowRight
+                size={20}
+                className=" absolute right-5 transition duration-500 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+              />
+            </Button>
           </div>
         </div>
       </Container>
