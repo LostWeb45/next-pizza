fa2346a22b1b843df8937cdf0e58025c3c14c146
LostWeb45 <kostik052005@gmail.com>
2025-03-21 17:07:18 +0300

    Поиск в фильтрации ингридиентов

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { FilterChecboxProps, FilterCheckbox } from "./filter-checkbox";
 import { Input } from "../ui";
+import { Value } from "@radix-ui/react-select";
 
 type Item = FilterChecboxProps;
 
 interface Props {
   title: string;
   items: Item[];
-  defaultItems?: Item[];
+  defaultItems: Item[];
   limit?: number;
   searchInputPlaceholder?: string;
   onChange?: (values: string[]) => void;
@@ -26,27 +27,54 @@ export const CheckboxGroup: React.FC<Props> = ({
   defaultValue,
   className,
 }) => {
+  const [showAll, setShowAll] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const list = showAll
+    ? items.filter((item) =>
+        item.text.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    : defaultItems?.slice(0, limit);
+
   return (
     <div className={cn(className)}>
       <p className="font-bold mb-3">{title}</p>
-      <div className="mb-5">
-        <Input
-          placeholder={searchInputPlaceholder}
-          className="ba-gray=50 border-none"
-        />
-      </div>
+
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className="ba-gray=50 border-none"
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
-        {items.map((item, index) => (
+        {list.map((item, index) => (
           <FilterCheckbox
             onCheckedChange={(ids) => console.log(ids)}
             checked={false}
-            key={String(item.value)}
+            key={index}
             value={item.value}
             text={item.text}
             endAdornment={item.endAdornment}
           />
         ))}
       </div>
+      {items.length > limit && (
+        <div className={showAll ? "border-t border-t-neutral-100 mt-4" : ""}>
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className="text-primary mt-3"
+          >
+            {showAll ? "Скрыть" : "+ Показать все"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
