e49869ef8e012a10c2a04ce42fa1a306b49df5ab
LostWeb45 <kostik052005@gmail.com>
2025-03-27 17:49:36 +0300

    Пиццы

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,12 @@
 import { hashSync } from "bcrypt";
 import { prisma } from "./prisma-client";
+import { categories, ingredients, products } from "./constants";
 
 async function up() {
   await prisma.user.createMany({
     data: [
       {
-        fullName: "User",
+        fullName: "User test",
         email: "s2iJL@example.com",
         password: hashSync("123456", 10),
         verified: new Date(),
@@ -20,9 +21,58 @@ async function up() {
       },
     ],
   });
+
+  await prisma.category.createMany({
+    data: categories,
+  });
+
+  await prisma.ingridient.createMany({
+    data: ingredients,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  const pizza1 = await prisma.product.create({
+    data: {
+      name: "Пепперони фреш",
+      image:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp",
+      categoryId: 1,
+      ingridients: {
+        connect: ingredients.slice(0, 5),
+      },
+    },
+  });
+
+  const pizza2 = await prisma.product.create({
+    data: {
+      name: "Сырная",
+      image:
+        "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp",
+      categoryId: 1,
+      ingridients: {
+        connect: ingredients.slice(5, 10),
+      },
+    },
+  });
+
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: "Чоризо фреш",
+      image:
+        "https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp",
+      categoryId: 1,
+      ingridients: {
+        connect: ingredients.slice(10, 40),
+      },
+    },
+  });
 }
 async function down() {
-  await prisma.$executeRaw`TRUNCATE TABLE "User" CASCADE;`;
+  await prisma.$executeRaw`DELETE FROM "User";`;
+  await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1;`;
 }
 async function main() {
   try {
