fcd80ef4b4ab5dd9d9fd1235bfc6f7e244bdbb10
LostWeb45 <kostik052005@gmail.com>
2025-03-30 13:33:21 +0300

    Поиск пицц

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -2,14 +2,17 @@ import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const query = request.nextUrl.searchParams.get("query") || '';
+  const query = request.nextUrl.searchParams.get("query") || "";
 
   const products = await prisma.product.findMany({
     where: {
       name: {
         contains: query,
+        mode: "insensitive",
       },
-    }
+    },
+    take: 5,
+  });
 
-  return NextResponse.json({});
+  return NextResponse.json(products);
 }
