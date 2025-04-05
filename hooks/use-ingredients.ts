fe6d343254ee393d19f819a145a18c71c4de43f7
LostWeb45 <kostik052005@gmail.com>
2025-04-05 13:53:49 +0300

    Вынос функционала в разные хуки

--- hooks/use-ingredients.ts
+++ hooks/use-ingredients.ts
@@ -1,24 +1,11 @@
 import { Api } from "@/services/api-client";
 import { Ingredient } from "@prisma/client";
 import React from "react";
-import { useSet } from "react-use";
 
-type IngredientItem = Pick<Ingredient, "id" | "name">;
+export const useIngredients = () => {
+  const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
 
-interface ReturnProps {
-  ingredients: IngredientItem[];
-  loading: boolean;
-  selectedIngredients: Set<string>;
-  onAddId: (id: string) => void;
-}
-
-export const useFilterIngredients = (): ReturnProps => {
-  const [ingredients, setIngredients] = React.useState<
-    ReturnProps["ingredients"]
-  >([]);
   const [loading, setLoading] = React.useState(true);
-
-  const [selectedIds, { toggle }] = useSet(new Set<string>([]));
 
   React.useEffect(() => {
     async function fetchIngredients() {
@@ -44,7 +31,5 @@ export const useFilterIngredients = (): ReturnProps => {
   return {
     ingredients,
     loading,
-    onAddId: toggle,
-    selectedIngredients: selectedIds,
   };
 };
