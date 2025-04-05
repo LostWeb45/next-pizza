5f7c8b35071f779239645b5f3bff64be00c8c932
LostWeb45 <kostik052005@gmail.com>
2025-04-05 12:16:19 +0300

    Сохранение параметров

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -34,9 +34,17 @@ export const Filters: React.FC<Props> = ({ className }) => {
   const { ingredients, loading, onAddId, selectedIngredients } =
     useFilterIngredients();
 
-  const [sizes, { toggle: toggleSizes }] = useSet(new Set<string>([]));
+  const [sizes, { toggle: toggleSizes }] = useSet(
+    new Set<string>(
+      searchParams.get("sizes") ? searchParams.get("sizes")?.split(",") : []
+    )
+  );
   const [pizzaTypes, { toggle: togglePizzaTypes }] = useSet(
-    new Set<string>([])
+    new Set<string>(
+      searchParams.get("pizzaTypes")
+        ? searchParams.get("pizzaTypes")?.split(",")
+        : []
+    )
   );
 
   const [prices, setPrice] = React.useState<PriceProps>({
